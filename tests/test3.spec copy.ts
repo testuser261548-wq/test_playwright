7b4e340c32bc411ea3db9c3689dec1bb284eaf12
testuser261548-wq <testuser261548@gmail.com>
2025-09-11 13:50:08 +0300

    dfasdf

--- tests/test3.spec copy.ts
+++ tests/test3.spec copy.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
+test('test3', { tag: ['@copytest3'] },async ({ page }) => {
   await page.goto('https://stage2user:RodeteQuakenDuttDreherAf@mini-de-awsint-m3.int.miniweb.eu-central-1.aws.bmw.cloud/de_DE/home.html');
   await page.getByRole('button', { name: 'Alle akzeptieren' }).click();
   await page.getByRole('button', { name: 'Modelle' }).click();
